8f7b7de3923517b870d2f594236dee731fa627af
Usman Arif <usmanarif.9219@gmail.com>
2024-05-30 23:57:12 +0500

    comment code

--- app/Http/Controllers/CvController.php
+++ app/Http/Controllers/CvController.php
@@ -19,21 +19,19 @@ class CvController extends Controller
         $pdf = SnappyPdf::loadView('my_cv')->setPaper('a4')->setOrientation('landscape')->setOption('margin-bottom', 0);
         return $pdf->download('usman_cv.pdf');
 
-        $snappy = App::make('snappy.pdf');
+        // $snappy = App::make('snappy.pdf');
 
-        $html = view('my_cv')->render();
+        // $html = view('my_cv')->render();
 
-        // $snappy->generateFromHtml($html, public_path('usman_cv.pdf'));
+        // $snappy->generate($html, public_path('usman_cv.pdf'));
 
-        $snappy->generate($html, public_path('usman_cv.pdf'));
-
-        return new Response(
-            $snappy->getOutputFromHtml($html),
-            200,
-            array(
-                'Content-Type'          => 'application/pdf',
-                'Content-Disposition'   => 'attachment; filename="123.pdf"'
-            )
-        );
+        // return new Response(
+        //     $snappy->getOutputFromHtml($html),
+        //     200,
+        //     array(
+        //         'Content-Type'          => 'application/pdf',
+        //         'Content-Disposition'   => 'attachment; filename="123.pdf"'
+        //     )
+        // );
     }
 }
